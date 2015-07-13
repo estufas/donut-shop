fbc81bd45ac06a83f1fdc6bcb2056a6f3db6cbc2
Stuart Hiller <hiller.stuart@gmail.com>
2015-07-13 16:27:32 -0700

    the Js is now populating the HTMl

--- shop.js
+++ shop.js
@@ -6,7 +6,9 @@ var DonutLocal = function(local, options) {
   this.minCust = options.minCust;
   this.maxCust = options.maxCust;
   this.avgPurch = options.avgPurch;
-  this.hoursOpen = 11;
+  this.opens = options.opens || 700;
+  this.closes = options.closes || 1800;
+  this.hoursOpen = (this.closes - this.opens)/100;
 };
 
 DonutLocal.prototype.avgHourCust = function() {
@@ -16,13 +18,33 @@ DonutLocal.prototype.avgHourCust = function() {
 DonutLocal.prototype.avgHourSale = function() {
   return (this.avgHourCust() * this.avgPurch);
 };
-DonutLocal.prototype.avgSales = function() {
-  return this.avgHourSale() * this.hoursOpen;
-};
+
+DonutLocal.prototype.render = function() {
+  var total = 0
+    for (var i = 0; i < this.hoursOpen; i++) {
+      total += this.avgHourSale();
+    };
+  var addDonutLocal = document.createElement('tr');
+  addDonutLocal.innerHTML = "<td>" + this.avgHourSale() + "</td>";
+  return addDonutLocal;
+}
+
+var main = document.getElementById('content');
 
 
 
 
 var capHill = new DonutLocal('Capital Hill', {minCust:4, maxCust:37, avgPurch:2})
+main.appendChild(capHill.render())
 
-console.log(capHill.avgSales());
+var southLakeU = new DonutLocal('South Lake Union', {minCust:9, maxCust:23, avgPurch:6.33})
+main.appendChild(capHill.render())
+
+var downTown = new DonutLocal('DownTown', {minCust:8, maxCust:43, avgPurch:4.5})
+main.appendChild(capHill.render())
+
+var wedgeWood = new DonutLocal('wedgeWood', {minCust:2, maxCust:28, avgPurch:1.25})
+main.appendChild(capHill.render())
+
+var ballard = new DonutLocal('Ballard', {minCust:8, maxCust:58, avgPurch:3.75})
+main.appendChild(capHill.render())
